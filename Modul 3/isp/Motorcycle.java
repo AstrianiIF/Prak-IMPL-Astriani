ba8d23327a0a9703eff17204ba2817da29d82523
astrianikomeri33 <astrianikomeri33@gmail.com>
2024-11-04 07:42:19 +0700

    Class Vehicle tanpa ISP

---
+++ Modul 3/isp/Motorcycle.java
@@ -0,0 +1,16 @@
+class Motorcycle implements VehicleInterface {
+   
+    @Override
+    public void drive() {
+    }
+    @Override
+    public void stop() {
+    }
+    @Override
+    public void refuel() {
+    }
+    // Can not be implemented
+    @Override
+    public void openDoors() {
+    }
+}
