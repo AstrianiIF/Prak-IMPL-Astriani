95b9043b5847f54d1d0f0d491e0c6619541d0906
astrianikomeri33 <astrianikomeri33@gmail.com>
2024-12-02 07:59:41 +0700

    Menambah latihan 1

---
+++ Modul 5/Tugas/KelasPengguna.java
@@ -0,0 +1,12 @@
+package counter;
+
+public class KelasPengguna {
+
+	protected String username;
+	protected String password;
+	
+	public void penyimpanan(String username, String password) {
+		
+	}
+	
+}
