457e2a3cdbc63b131ac84f99684783d42e587a31
astrianikomeri33 <astrianikomeri33@gmail.com>
2024-12-02 07:42:27 +0700

    Menambah Good dan Bad Comments

---
+++ Modul 5/GoodComments.java
@@ -0,0 +1,9 @@
+package counter;
+
+public class GoodComments {
+
+	// Copyright (C) 2011 by Osoco. All rights reserved.
+	// Released under the terms of the GNU General Public License
+	// version 2 or later.
+	
+}
